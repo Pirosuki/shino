270e4e0ea0bd126ec36c1db67602c31585d586e4
Pirosuki <36814481+Pirosuki@users.noreply.github.com>
2022-08-13 19:03:56 +0200

    Modified to actually work

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -3,14 +3,39 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pause")
-        .setDescription("Pause any current audio playback"),
+        .setDescription("Pause current audio playback"),
 
     async execute(interaction) {
-        let player = ""; // Fix this
+        // Get voice connection
+        const connection = getVoiceConnection(interaction.guild.id);
 
-        player.pause();
+        // Get audio player
+        const player = connection.state.subscription.player;
 
-        await interaction.reply("Paused audio playback");
-        console.log("[" + interaction.guild.name + "] " + interaction.user.tag + " paused audio playback");
+        // Checks that the connection is valid and that the connection matches between user and bot
+        if (!connection || interaction.member.voice.channelId !== connection.joinConfig.channelId) {
+            // If not then return error
+            await interaction.reply({ content: "We have to be in the same voice channel for you to use that command", ephemeral: true });
+        }
+        // Checks if player is already paused
+        else if (player.Paused) {
+            // If it is then return error
+            await interaction.reply({ content: "Audio playback is already paused", ephemeral: true });
+        }
+        // Check if player is idle
+        else if (player.Idle) {
+            // If it is then return error
+            await interaction.reply({ content: "Nothing is currently playing", ephemeral: true });
+        }
+        else {
+            // Pause audio playback
+            player.pause();
+
+            // Reply to user
+            await interaction.reply("Playback paused");
+
+            // Log to console
+            console.log("[" + interaction.guild.name + "] " + interaction.user.tag + " paused audio playback.");
+        }
     },
 };
--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,18 +1,38 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("stop")
-        .setDescription("Stop current music playback and have shino disconnect from voice"),
+        .setDescription("Stop current audio playback and clear queue"),
 
     async execute(interaction) {
-        // Player should be automatically deleted once the voiceconnection is gone
-        // Destroys voice connection
+        // Get voice connection
         const connection = getVoiceConnection(interaction.guild.id);
-        connection.destroy();
 
-        await interaction.reply("response");
-        console.log("[" + interaction.guild.name + "] " + interaction.user.tag + " stopped music playback");
+        // Get audio player
+        const player = connection.state.subscription.player;
+
+        // Checks that the connection is valid and that the connection matches between user and bot
+        if (!connection || interaction.member.voice.channelId !== connection.joinConfig.channelId) {
+            // If not then return error
+            await interaction.reply({ content: "We have to be in the same voice channel for you to use that command", ephemeral: true });
+        }
+        // Check if player is idle
+        else if (player.Idle) {
+            // If it is then return error
+            await interaction.reply({ content: "Nothing is currently playing", ephemeral: true });
+        }
+        else {
+            // Stop audio playback
+            player.stop();
+
+            // Clear queue // FIX //
+
+            // Reply to user
+            await interaction.reply("Playback stopped and queue (not really) cleared");
+
+            // Log to console
+            console.log("[" + interaction.guild.name + "] " + interaction.user.tag + " stopped audio playback.");
+        }
     },
 };
